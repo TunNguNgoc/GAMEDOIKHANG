83e5ee0b18599b793b079bfb71bcf24a4f2d6557
tunngungoc <phrafttanyfw@gmail.com>
2023-03-13 15:00:36 +0700

    JS 1st

---
+++ js/Class.js
@@ -0,0 +1,11 @@
+const cancleTime = 10
+var currentTime = 0
+var nowtime =0
+
+// Milliseconds since Jan 1, 1970, 00:00:00.000 GMT
+
+
+function getTimestampInSeconds () {
+  return Math.floor(Date.now() / 1000)
+
+}
